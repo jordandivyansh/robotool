1650299a7bd550c75931d190b45eb40956d83f0d
jordandivyansh <divyanshkrit@gmail.com>
2020-04-25 09:31:25 +0530

    adding files

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = ({name, email, id}) =>{
+	return(
+		<div className='tc bg-black dib br3 pa3 ma2 grow bw2 shadow-5 yellow { color: var(--yellow); }
+		b--dashed { border-style: dashed; }'>
+		 <img  alt='Robo' src={'https://robohash.org/${id}?size=100x100'}/>
+		 <div>
+		  <h2>{name}</h2>
+		  <p>{email}</p>
+		 </div>
+		</div>
+		);
+}
+export default Card;
---
+++ src/robots.js
@@ -0,0 +1,69 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Aayush',
+    username: 'Bret',
+    email: 'aayush@lobby.io'
+  },
+  {
+    id: 2,
+    name: 'Amit',
+    username: 'Antonette',
+    email: 'amit@lobby.io'
+  },
+  {
+    id: 3,
+    name: 'Ankit',
+    username: 'Samantha',
+    email: 'ankit@lobby.io'
+  },
+  {
+    id: 4,
+    name: 'Ayush',
+    username: 'Karianne',
+    email: 'aayush@lobby.io'
+  },
+  {
+    id: 5,
+    name: 'Dipit',
+    username: 'Kamren',
+    email: 'dipit@lobby.io'
+  },
+  {
+    id: 6,
+    name: 'Jordan',
+    username: 'Leopoldo_Corkery',
+    email: 'jordan@lobby.io'
+  },
+  {
+    id: 11,
+    name: 'Harshal',
+    username: 'Moriah.Stanton',
+    email: 'harshal@lobby.io'
+  },
+  {
+    id: 7,
+    name: 'Kamal',
+    username: 'Elwyn.Skiles',
+    email: 'kamal@lobby.io'
+  },
+  {
+    id: 8,
+    name: 'DVD',
+    username: 'Maxime_Nienow',
+    email: 'dvd@lobby.io'
+  },
+  {
+    id: 9,
+    name: 'Shivam',
+    username: 'Delphine',
+    email: 'shivam@lobby.io'
+  },
+  {
+    id: 10,
+    name: 'Vishnu',
+    username: 'Moriah.Stanton',
+    email: 'vishnu@lobby.io'
+  }
+
+];
